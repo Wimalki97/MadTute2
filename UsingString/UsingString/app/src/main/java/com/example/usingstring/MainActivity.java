eabf77836cecc6b2ded60b403cd0bb85836ed2f5
Wimalki97 <67803441+Wimalki97@users.noreply.github.com>
2020-08-08 23:00:32 +0530

    commiting

---
+++ UsingString/UsingString/app/src/main/java/com/example/usingstring/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.usingstring;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.DragStartHelper;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txTVMsg2=findViewById(R.id.TvMsg2);
+        txTVMsg2.setText(R.string.Msg2);
+
+        Log.i("Lifecycle","OnCreate() invoked");
+    }
+    @Override
+    protected void    onStart(){
+        super.onStart();
+        Log.i("Lifecycle","OnStart called....");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Lifecycle","onResume called....");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Lifecycle","onPause called....");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Lifecycle","onStop called....");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Lifecycle","onDestroy called....");
+    }
+}
